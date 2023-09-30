c16829ed2e8bfb506b947c057af4706285145dba
Braian Emanuel Tito <105461834+braian004@users.noreply.github.com>
2023-09-30 12:59:33 -0300

    Delete 11_listas_metodos.py

--- 11_listas_metodos.py
+++
@@ -1,9 +0,0 @@
-lenguaje = ["python","Rubi","PHP","Javascrip","Java"]
-lenguaje.insert(3,"Go")
-lenguaje.insert(0,"C")
-lenguaje.remove("Rubi")
-print("C"in lenguaje)
-print(lenguaje)
-# lenguaje.clear()}
-
-print(len(lenguaje))

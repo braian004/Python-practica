223967821bf597152f325dbf2ea848eb4f84153c
Braian Emanuel Tito <105461834+braian004@users.noreply.github.com>
2023-09-30 12:59:52 -0300

    Delete 08_if_flojo_de_control.py

--- 08_if_flojo_de_control.py
+++
@@ -1,8 +0,0 @@
-puntaje = input('write your note:')
-if int(puntaje) >= 95:
-    print("aprovo con honores")
-elif int(puntaje) > 40:
-    print("aprovo")
-else :
-    print("desaprovo")    
-    44

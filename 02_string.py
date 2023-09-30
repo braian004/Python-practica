a4d70980c8a80ca2f5d7bd16a407c2fd5f19903e
Braian Emanuel Tito <105461834+braian004@users.noreply.github.com>
2023-09-30 12:53:56 -0300

    Delete 02_string.py

--- 02_string.py
+++
@@ -1,9 +0,0 @@
-text = "hello world"
-print (text.upper())
-print (text.lower())
-print (text.lower())
-print (text.replace('d',"dooooo"))
-newTexto = text.replace("hello","chang_the_string")
-print(newTexto)
-print(text.replace("hello","chang the string"))
-print ("Mon" in text)

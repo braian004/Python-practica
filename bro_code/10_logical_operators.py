e1f0825ec4cd75d67fa3c36eb86c14e9cb888f40
Braian Emanuel Tito <105461834+braian004@users.noreply.github.com>
2023-10-02 15:19:03 -0300

    Add files via upload

---
+++ bro_code/10_logical_operators.py
@@ -0,0 +1,14 @@
+# temp = int(input("what is the temperature outside?:"))
+# if temp >= 0 and temp <= 30:
+#     print("the temperature is good today!")
+#     print("go outside!")
+# elif temp < 0 or temp > 30:
+#     print("the temperature is bad today")
+#     print("the inside!")
+temp = float(input("what is the temperature outside?:"))
+if temp >= 0 and temp <= 30:
+    print("the temperature is good today!")
+    print("out side")
+elif not temp < 0 or temp < 30:
+    print("the temperature is bad today!")    
+    print("stay inside")
---
+++ learn_python.py/calculadora.py
@@ -0,0 +1,16 @@
+num1 = float(input("enter a number:"))
+
+
+num2 = float(input("enter anoter number"))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+division_result = num1 / num2
+multiplication_result = num1 * num2
+
+
+
+print(f"{addition_result}")
+print(f"{subtraction_result}")
+print(f"{division_result}")
+print(f"{multiplication_result}")                                                                                                                    

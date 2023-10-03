bb0377e5889c9da0c2aeb98b58f8a96d6ad06d5b
braina004 <braianemanueltito@gmail.com>
2023-10-03 17:43:21 -0300

    10-3-2023

--- hola_mundo/funtion/01_funtion.py
+++ hola_mundo/funtion/01_funtion.py
@@ -2,5 +2,5 @@ def newfuntion(name):# name => PARAMETRO
     print("hola")
     print(f"bienvenido satanas{name}")
 newfuntion("name")#name => ARGUMENTO Q
-newfuntion("CJJFJF")
+newfuntion("CJJFJFs")
 

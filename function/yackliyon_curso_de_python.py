7ade4e674cde6f8de653e25a1f0f6d1b1437e6e6
braina004 <braianemanueltito@gmail.com>
2023-10-04 12:02:52 -0300

    10-4-2023

--- function/yackliyon_curso_de_python.py
+++ function/yackliyon_curso_de_python.py
@@ -15,11 +15,52 @@
 # m = 23
 # function()
 
-def dato():
-    n=2
-    print(n)
+# def dato():
+#     n=2
+#     print(n)
 # dato()
 
-j = 567
-dato()
-print(j)
+# j = 567
+# dato()
+# print(j)
+
+# def suma(a,b):
+#     return a+b  
+# respuesta = suma(4,6)
+# print(respuesta)
+
+'''separa una lista '''
+'''pares e inpares'''
+ejemplo = [3,7,9,5,3,7,3,12]
+# def separador(lista):
+#     # lista.short # => para qu este ordenado
+#     pares = []
+#     inpares = []
+#     for i in lista:
+#         if i % 2 == 0:
+#             pares.append(i)
+#         else:
+#             inpares.append(i)
+#     return pares, inpares
+# pares,inpares = separador(ejemplo)
+# print(pares)
+# print(inpares)
+
+
+
+
+
+
+def separar_lista(lista):
+    # lista.sort()
+    pares = []
+    inpares = []
+    for i in lista:
+        if i % 2 == 0:
+            pares.append(i)
+        else:
+            inpares.append(i)
+    return pares,inpares
+pares,inpares = separar_lista(ejemplo)
+print(pares)
+print(inpares)

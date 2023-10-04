1e92a6ca196f287ad229a29311f42ade30a24fe5
braina004 <braianemanueltito@gmail.com>
2023-10-04 10:43:23 -0300

    add yackliyon

--- function/yackliyon_curso_de_python.py
+++ function/yackliyon_curso_de_python.py
@@ -1,3 +1,25 @@
-def suma_tres (n):
-    print (n+3)
-suma_tres(4)
+# def suma_tres(n):
+#     print (n+3)
+# suma_tres(4)
+
+
+# def tabla_de_multiplicar(n):
+#     for i in range(10):
+#         print(f"2 x {i} = {i*n}")
+# tabla_de_multiplicar(2)
+
+# n = 56
+# def function():
+#     print(n)
+#     print(m)
+# m = 23
+# function()
+
+def dato():
+    n=2
+    print(n)
+# dato()
+
+j = 567
+dato()
+print(j)

01e0fba30e4c44849d1d1ac18a78d8d573717a3b
braina004 <braianemanueltito@gmail.com>
2023-12-11 00:07:49 -0300

    12-11-23

--- webscraping/ee-uu-comapny/string.py
+++ webscraping/ee-uu-comapny/string.py
@@ -7,7 +7,10 @@ url = 'https://www.visitingangels.com/senior-homecare-bend-oregon-97702_436'
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml')
 data = soup.find_all("div", class_="fran-info")
+data = [i.text for i in data]
 
 print(data)
+df = pd.DataFrame(data)
+df
 
 # ation in the united states  location   bussines name   owner name   adsdress   CONTACTfrom bs4 import BeautifulSoup
